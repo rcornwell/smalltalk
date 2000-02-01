e810c9bd96eb49f80f2f3658fd2af9c1a17b5ca0
Richard Cornwell <rcornwell@github.com>
2000-02-01 18:09:52 +0000

    Added stack checking code. Changed class variables to an Array. Moved image start up to here.

--- init.c
+++ init.c
@@ -2,13 +2,16 @@
 /*
  * Smalltalk interpreter: Initialize basic Known and builtin objects.
  *
- * $Log: $
+ * $Log: init.c,v $
+ * Revision 1.1  1999/09/02 15:57:59  rich
+ * Initial revision
+ *
  *
  */
 
 #ifndef lint
 static char        *rcsid =
-	"$Id: $";
+	"$Id: init.c,v 1.1 1999/09/02 15:57:59 rich Exp rich $";
 
 #endif
 
@@ -38,6 +41,7 @@ static char        *rcsid =
 #include "dump.h"
 #include "symbols.h"
 #include "parse.h"
+#include "image.h"
 
 struct _class_template {
 	    Objptr              object;
@@ -49,15 +53,13 @@ struct _class_template {
 	} class_template[] = {
     /* Order is important for reference counting. */
     { ClassClass, CLASS_PTRS, CLASS_SIZE, BehaviorClass, "Class",
-	    "superclass subclasses methoddict flags"
-	    " name vars comment category classvars pool"
+	    "name vars comment category classvars pool methcats"
     },
-    { BehaviorClass, CLASS_PTRS, CLASS_NAME + 1, ObjectClass, "Behavior",
+    { BehaviorClass, CLASS_PTRS, CLASS_NAME, ObjectClass, "Behavior",
 	    "superclass subclasses methoddict flags"
     },
     { MetaClass, CLASS_PTRS, META_INSTCLASS + 1, ClassClass, "MetaClass",
-	    "superclass subclasses methoddict flags"
-	    " name vars comment category classvars pool instanceClass"
+	    "instanceClass"
     },
     { ObjectClass, 0, 0, NilPtr, "Object", NULL },
     { StringClass, CLASS_BYTE | CLASS_INDEX, 0, ObjectClass, "String", NULL },
@@ -78,7 +80,7 @@ struct _class_template {
     { TrueClass, 0, 0, BooleanClass, "True", NULL },
     { FalseClass, 0, 0, BooleanClass, "False", NULL },
     { LinkClass, CLASS_PTRS, 1, ObjectClass, "Link", "next" },
-    { SymLinkClass, CLASS_PTRS, 2, LinkClass, "Symbol", "next value" },
+    { SymLinkClass, CLASS_PTRS, 2, LinkClass, "Symbol", "value" },
     { BooleanClass, 0, 0, ObjectClass, "Boolean", NULL },
     { CompiledMethodClass, CLASS_INDEX, 2, ObjectClass, "CompiledMethod",
 	    "description header"
@@ -87,7 +89,7 @@ struct _class_template {
 	    "sourceFile sourcePos category"
     },
     { IdentityDictionaryClass, CLASS_PTRS | CLASS_INDEX, 2, DictionaryClass,
-	    "IdentityDictionary", "tally values"
+	    "IdentityDictionary", "values"
     },
     { DictionaryClass, CLASS_PTRS | CLASS_INDEX, 1, SSetClass, "Dictionary",
 	    "tally"
@@ -159,15 +161,12 @@ name_classes()
 	name = MakeString(ct->name);
 	Set_object(op, CLASS_NAME, name);
 	assoc = create_association(internString(ct->name), op);
-	if (ct->instvars != NULL)
-	    Set_object(op, CLASS_VARS, MakeString(ct->instvars));
-	else
-	    Set_object(op, CLASS_VARS, MakeString(""));
+        Set_object(op, CLASS_VARS, NilPtr);
 	Set_object(op, SUPERCLASS, ct->superclass);
 
 	/* Make metaclass to follow class */
 	meta = create_new_object(MetaClass, 0);
-	Set_integer(meta, CLASS_FLAGS, (ct->size * 8) + (ct->flags / 2));
+	Set_integer(op, CLASS_FLAGS, (ct->size * 8) + (ct->flags / 2));
 	Set_object(meta, CLASS_NAME, name);
 	Set_object(meta, META_INSTCLASS, op);
 	Set_integer(meta, CLASS_FLAGS, (CLASS_SIZE * 8) + (CLASS_PTRS / 2));
@@ -186,6 +185,55 @@ name_classes()
 
     /* Lastly fix Objects Metaclass's superClass to Class */
     Set_object(class_of(ObjectClass), SUPERCLASS, ClassClass);
+}
+
+/*
+ * Install instance variables to classes.
+ */
+static void
+instvar_classes()
+{
+    int                 i;
+    Objptr              op;
+    Objptr		sup;
+    char                *ptr, *ptr2, *str, c;
+    Objptr		array;
+    int			count;
+    struct _class_template *ct;
+
+    /* Finish initializing classes */
+    for (i=0; i<(sizeof(class_template)/sizeof(struct _class_template)); i++) {
+	ct = &class_template[i];
+	op = ct->object;
+
+	/* Make a copy of instance names so we can modify them */
+        if (ct->instvars == NULL) 
+	    continue;
+	if ((str = strsave(ct->instvars)) == NULL)
+	   /* Should error here, but just punt, error will show up real soon */
+	    continue;
+
+	sup = get_pointer(op, SUPERCLASS);
+	count = ct->size - (get_integer(sup, CLASS_FLAGS) / 8);
+        array = create_new_object(ArrayClass, count * sizeof(Objptr));
+        Set_object(op, CLASS_VARS, array);
+	if (count == 0)
+	   continue;
+        ptr2 = ptr = str;
+	count = 0;
+        do {
+	    c = *ptr++;
+	    if (c == ' ' || c == '\0') {
+	       if (ptr2 != ptr) {
+		    *--ptr = '\0';
+	    	    Set_object(array, count++, MakeString(ptr2));
+		    *ptr++ = c;
+	            ptr2 = ptr;
+	       }
+            }
+        } while (c != '\0');
+	free(str);
+    }
 }
 
 /*
@@ -243,11 +291,12 @@ loadPrimitives()
  * Load initial bootstrap objects into object memory.
  */
 void
-smallinit()
+smallinit(int otsize)
 {
     Objptr              temp;
     int                 i;
 
+    new_objectable(otsize);
     noreclaim = TRUE;		/* Don't allow a reclaim until done */
 
    /*
@@ -263,6 +312,7 @@ smallinit()
     create_object(CannotReturnSelector, 0, SymLinkClass, 2 * sizeof(Objptr));
     create_object(MustBeBooleanSelector, 0, SymLinkClass, 2 * sizeof(Objptr));
     create_object(UnknownClassVar, 0, SymLinkClass, 2 * sizeof(Objptr));
+    create_object(InterpStackFault, 0, SymLinkClass, 2 * sizeof(Objptr));
     create_object(SpecialSelectors, CLASS_PTRS | CLASS_INDEX, ArrayClass,
 		  32 * sizeof(Objptr));
     create_object(CharacterTable, CLASS_PTRS | CLASS_INDEX, ArrayClass,
@@ -286,6 +336,7 @@ smallinit()
     rootObjects[SELECT1] = CannotReturnSelector;
     rootObjects[SELECT2] = MustBeBooleanSelector;
     rootObjects[SELECT3] = UnknownClassVar;
+    rootObjects[SELECT4] = InterpStackFault;
 
    /* Rebuild free list */
     rebuild_free();
@@ -297,6 +348,7 @@ smallinit()
     makeSymbol(CannotReturnSelector, "cannotReturn");
     makeSymbol(MustBeBooleanSelector, "mustBeBoolean");
     makeSymbol(UnknownClassVar, "unknownClassVar");
+    makeSymbol(InterpStackFault, "interpStackFault");
    /* Build Character table */
     for (i = 0; i < 256; i++) {
 	temp = create_new_object(CharacterClass, 0);
@@ -319,8 +371,8 @@ smallinit()
 	create_association(internString("Processor"),
 				 SchedulerAssociationPointer));
     AddSelectorToDictionary(SmalltalkPointer,
-	create_association(internString("sourceFiles"),
-				 create_new_object(ArrayClass, 4)));
+	create_association(internString("initSourceFile"),
+				 create_new_object(ArrayClass, 3)));
     AddSelectorToDictionary(SmalltalkPointer,
 	create_association(internString("charsymbols"), CharacterTable));
 
@@ -328,8 +380,63 @@ smallinit()
 	if (specialSelector[i] != NULL)
 	    Set_object(SpecialSelectors, i,
 		       internString(specialSelector[i]));
+
+    /* Install class instance variable names now */
+    instvar_classes();
     loadPrimitives();
     noreclaim = FALSE;		/* Ok, safe now to free unused junk */
+}
+
+void
+load_file(char *str)
+{
+    int                 sstack;
+    Objptr              caller;
+
+    noreclaim = TRUE;
+   /* Load old image into memory */
+    if (!load_image(str))
+	error("Failed to load image");
+ 
+   /* Load known pointers */
+    rootObjects[SMALLTLK] = SmalltalkPointer;
+    rootObjects[SYMTAB] = SymbolTable;
+    rootObjects[CHARTAB] = CharacterTable;
+    rootObjects[OBJECT] = ObjectClass;
+    rootObjects[SPECSEL] = SpecialSelectors;
+    rootObjects[TRUEOBJ] = TruePointer;
+    rootObjects[FALSEOBJ] = FalsePointer;
+    rootObjects[SELECT0] = DoesNotUnderstandSelector;
+    rootObjects[SELECT1] = CannotReturnSelector;
+    rootObjects[SELECT2] = MustBeBooleanSelector;
+    rootObjects[SELECT3] = UnknownClassVar;
+    rootObjects[SELECT4] = InterpStackFault;
+    rootObjects[CURCONT] = current_context;
+    newContextFlag = 1;
+
+   /* Inlined return of False. */
+
+   /* Find out who original sender was. */
+    caller = get_pointer(current_context, BLOCK_CALLER);
+    sstack = get_integer(caller, BLOCK_SP);
+
+   /* Remove arguments. */
+    Set_object(caller, sstack++, NilPtr);
+    Set_object(caller, sstack++, NilPtr);
+
+   /* Push False value */
+    Set_object(caller, --sstack, FalsePointer);
+    Set_integer(caller, BLOCK_SP, sstack);
+
+    object_decr_ref(current_context);
+   /* Mark caller as current */
+    rootObjects[CURCONT] = current_context = caller;
+    rebuild_free();
+    
+    noreclaim = FALSE;
+    reclaimSpace();
+
+    interp();
 }
 
 void
@@ -346,41 +453,43 @@ parsefile(char *str)
     if ((fp = new_file(str, "r")) == NilPtr)
 	return;
 
-    ptr = get_chunk(fp);
-    while ((ptr != NULL)) {
-	if (*ptr != '\0') {
-	    dump_string(ptr);
-            meth = CompileForExecute(ptr);
-
-            header = get_pointer(meth, METH_HEADER);
-            stacksize = StackOf(header) + TempsOf(header);
-
-            /* Initialize a new method. */
-            newContext = create_new_object(MethodContextClass, stacksize);
-	    object_incr_ref(newContext);
-    	    rootObjects[CURCONT] = newContext;
-            Set_object(newContext, BLOCK_SENDER, newContext);
-            Set_integer(newContext, BLOCK_IP, 
-	    	sizeof(Objptr) *(LiteralsOf(header) + METH_LITSTART));
-            Set_integer(newContext, BLOCK_SP,
-            	size_of(newContext) / sizeof(Objptr));
-            Set_object(newContext, BLOCK_METHOD, meth);
-            Set_integer(newContext, BLOCK_IIP, 0);
-            Set_object(newContext, BLOCK_REC, fp);
-            Set_integer(newContext, BLOCK_ARGCNT, 0);
-            current_context = newContext;
-            newContextFlag = 1;
-	    rootObjects[TEMP1] = NilPtr;
-            interp();
-	    /* Break cycle */
-            Set_object(newContext, BLOCK_SENDER, NilPtr);
-	    object_decr_ref(newContext);
-	    if (filein) 
-		fileinMethods(fp);
-	} else
-	    free(ptr);
+    while (TRUE) {
 	filein = peek_for(fp, '!');
-	ptr = get_chunk(fp);
+        ptr = get_chunk(fp);
+	if (ptr == NULL || *ptr == '\0') {
+	    if (ptr != NULL)
+		free(ptr);
+	    break;
+	}
+	show_source(ptr);
+        meth = CompileForExecute(ptr);
+
+        header = get_pointer(meth, METH_HEADER);
+        stacksize = StackOf(header) + TempsOf(header);
+
+        /* Initialize a new method. */
+        newContext = create_new_object(MethodContextClass, stacksize);
+	object_incr_ref(newContext);
+    	rootObjects[CURCONT] = newContext;
+        Set_object(newContext, BLOCK_SENDER, newContext);
+        Set_integer(newContext, BLOCK_IP, 
+		sizeof(Objptr) *(LiteralsOf(header) + METH_LITSTART));
+        Set_integer(newContext, BLOCK_SP,
+            	size_of(newContext) / sizeof(Objptr));
+        Set_object(newContext, BLOCK_METHOD, meth);
+        Set_integer(newContext, BLOCK_IIP, 0);
+        Set_object(newContext, BLOCK_REC, fp);
+        Set_integer(newContext, BLOCK_ARGCNT, 0);
+        current_context = newContext;
+        newContextFlag = 1;
+	rootObjects[TEMP1] = NilPtr;
+        interp();
+	/* Break cycle */
+        Set_object(newContext, BLOCK_SENDER, NilPtr);
+	object_decr_ref(newContext);
+	if (filein) 
+	    fileinMethods(fp);
+	free(ptr);
     }
     close_buffer(fp);
 }
@@ -391,17 +500,18 @@ fileinMethods(Objptr fp)
     char	*ptr;
     int		position;
 
-    position = get_pointer(fp, FILEPOS);
+    position = get_integer(fp, FILEPOS);
     ptr = get_chunk(fp);
-    while ((ptr != NULL)) {
-	if (*ptr != '\0') {
-	    dump_string(ptr);
-	    CompileForClass(ptr, compClass, compCatagory, position);
-	} else {
-	    free(ptr);
-            break;
-	}
-        position = get_pointer(fp, FILEPOS);
+    while (ptr != NULL && *ptr != '\0') {
+	show_source(ptr);
+	CompileForClass(ptr, compClass, compCatagory, position);
+	free(ptr);
+        position = get_integer(fp, FILEPOS);
 	ptr = get_chunk(fp);
     }
+    if (ptr != NULL)
+	free(ptr);
 }
+
+
+
