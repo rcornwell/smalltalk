1561fe4a49b415ce913b3c15912520bca533148a
Richard Cornwell <rcornwell@github.com>
2000-03-04 22:25:21 +0000

    Converted local references to static

--- parse.h
+++ parse.h
@@ -1,37 +1,18 @@
 /*
  * Smalltalk interpreter: Parser.
  *
- * $Id: parse.h,v 1.1 1999/09/02 15:57:59 rich Exp rich $
+ * $Id: parse.h,v 1.2 2000/02/01 18:10:00 rich Exp rich $
  *
  * $Log: parse.h,v $
+ * Revision 1.2  2000/02/01 18:10:00  rich
+ * Routines now propage error back to caller.
+ *
  * Revision 1.1  1999/09/02 15:57:59  rich
  * Initial revision
  *
  *
  */  
 
-void                initcompile();
 void		    AddSelectorToClass(Objptr, char *, Objptr, Objptr, int);
 int                 CompileForClass(char *, Objptr, Objptr, int);
 Objptr              CompileForExecute(char *);
-void                messagePattern();
-void                keyMessage();
-void                doTemps();
-int		    isVarSep();
-void                doBody();
-void                doExpression();
-int                 doTerm();
-int                 parseBinary();
-int                 nameTerm(Symbolnode);
-int                 doContinue(int);
-int                 keyContinue(int);
-int                 doBinaryContinue(int);
-int                 doUnaryContinue(int);
-int                 doBlock();
-int                 optimBlock();
-int                 doIfTrue();
-int                 doIfFalse();
-int                 doWhileTrue();
-int                 doWhileFalse();
-int                 doAnd();
-int                 doOr();
