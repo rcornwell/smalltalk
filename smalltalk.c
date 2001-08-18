71d59956f47f9940fab9c769d269e80b0ea0b2ac
Richard Cornwell <rcornwell@github.com>
2001-08-18 16:17:02 +0000

    Moved windows code to win32.c. Made main generic between windows and unix.

--- smalltalk.c
+++ smalltalk.c
@@ -2,6 +2,9 @@
  * Smalltalk interpreter: Main routine.
  *
  * $Log: smalltalk.c,v $
+ * Revision 1.5  2001/07/31 14:09:49  rich
+ * Fixed to compile under cygwin.
+ *
  * Revision 1.4  2000/08/27 00:59:21  rich
  * Changed default grow size to 512k.
  *
@@ -19,7 +22,7 @@
 
 #ifndef lint
 static char        *rcsid =
-	"$Id: smalltalk.c,v 1.4 2000/08/27 00:59:21 rich Exp rich $";
+	"$Id: smalltalk.c,v 1.5 2001/07/31 14:09:49 rich Exp rich $";
 #endif
 
 #include "smalltalk.h"
@@ -27,264 +30,15 @@ static char        *rcsid =
 #include "object.h"
 #include "interp.h"
 #include "fileio.h"
+#include "system.h"
 #include "dump.h"
 
 char               *imagename = NULL;
 char               *loadfile = NULL;
 int                 defotsize = 512;
+char		   *progname = NULL;
 
 static int          getnum(char *, int *);
-
-#ifdef WIN32
-static void         CreateWindows(HINSTANCE, HWND, LPARAM);
-static void         DoWinCommand(HINSTANCE, HWND, WPARAM);
-static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-static char        *szAppName = "SmallTalk";
-static char        *getarg(char **);
-static int          parseCmd(char *);
-
-/*
- * Create main window.
- */
-int                 WINAPI
-WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-		    PSTR szCmdLine, int iCmdShow)
-{
-    HWND                hwnd;
-    HACCEL              hAccel;
-    MSG                 msg;
-    WNDCLASSEX          wndclass;
-
-   /* Initialize class structure */
-    wndclass.cbSize = sizeof(wndclass);
-    wndclass.style = CS_HREDRAW | CS_VREDRAW;
-    wndclass.lpfnWndProc = WndProc;
-    wndclass.cbClsExtra = 0;
-    wndclass.cbWndExtra = 0;
-    wndclass.hInstance = hInstance;
-    wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(SMALLTALK));
-    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wndclass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
-    wndclass.lpszMenuName = szAppName;
-    wndclass.lpszClassName = szAppName;
-    wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(SMALLTALK));
-
-   /* Create Window */
-    RegisterClassEx(&wndclass);
-
-    hwnd = CreateWindow(szAppName, "Smalltalk", WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, CW_USEDEFAULT, 500, 400,
-			NULL, NULL, hInstance, szCmdLine);
-
-   /* Initialize rest */
-    ShowWindow(hwnd, iCmdShow);
-    UpdateWindow(hwnd);
-
-    hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(SMALLTALK));
-
-   /* Make window loop */
-    while (GetMessage(&msg, NULL, 0, 0)) {
-	TranslateMessage(&msg);
-	DispatchMessage(&msg);
-    }
-    return msg.wParam;
-}
-
-/*
- * Main window proceedure.
- */
-static LRESULT      CALLBACK
-WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
-{
-    static HINSTANCE    hInst;
-
-    switch (iMsg) {
-    case WM_CREATE:
-	hInst = ((LPCREATESTRUCT) lParam)->hInstance;
-
-       /* Process the arguments */
-	if (!parseCmd(GetCommandLine())) {
-	    SendMessage(hwnd, WM_CLOSE, 0, 0);
-	    return 0;
-	}
-
-	CreateWindows(hInst, hwnd, lParam);
-
-       /* If there is a image file, load it */
-	if (imagename != NULL)
-	    load_file(imagename);
-	else {
-	   /* Build new system */
-	    smallinit(defotsize);
-	   /* Compile into system */
-	    if (loadfile != NULL)
-		parsefile(loadfile);
-	}
-
-	return 0;
-
-    case WM_COMMAND:
-	DoWinCommand(hInst, hwnd, wParam);
-
-	return 0;
-
-#if 0
-    case WM_CLOSE:
-	return 0;
-#endif
-
-    case WM_DESTROY:
-    	close_files();
-	PostQuitMessage(0);
-	return 0;
-
-    }
-    return DefWindowProc(hwnd, iMsg, wParam, lParam);
-}
-
-static void
-CreateWindows(HINSTANCE hInst, HWND hwnd, LPARAM lParam)
-{
-
-}
-
-static void
-DoWinCommand(HINSTANCE hInst, HWND hwnd, WPARAM wParam)
-{
-    char                buffer[MAX_PATH];
-
-   /* Messages from menus */
-    switch (LOWORD(wParam)) {
-    case IDM_NEW:
-	break;
-
-    case IDM_OPEN:
-	break;
-
-    case IDM_SAVE:
-       /* fall through */
-    case IDM_SAVEAS:
-	break;
-
-    case IDM_EXIT:
-	SendMessage(hwnd, WM_CLOSE, 0, 0);
-	break;
-
-    case IDM_HELP:
-       /* Convert our module name into a help file name */
-	strcpy(buffer, szAppName);
-	strcat(buffer, ".HLP");
-	WinHelp(hwnd, buffer, HELP_CONTENTS, (DWORD) 0);
-	break;
-
-    case IDM_ABOUT:
-	AboutBox(hwnd, hInst, szAppName, "#1");
-	break;
-
-    }
-}
-
-/*
- * Grab next argument from command string.
- */
-static char        *
-getarg(char **str)
-{
-    char               *ptr = *str;
-    char                c;
-    char               *arg;
-
-   /* Skip leading blanks */
-    while (*ptr == ' ' || *ptr == '\t')
-	ptr++;
-    if (*ptr == '\0')
-	return NULL;
-
-   /* Pointer to start of argument */
-    arg = ptr;
-
-   /* For "'d strings, skip till end of string */
-    if (*arg == '"') {
-	for (arg = ++ptr; *ptr != '"' && *ptr != '\0'; ptr++) ;
-    } else {
-       /* Otherwise, skip till next white space */
-	for (; *ptr != ' ' && *ptr != '\t' && *ptr != '\n'
-	     && *ptr != '\r' && *ptr != '\0'; ptr++) ;
-    }
-
-   /* Save current character */
-    c = *ptr;
-
-   /* Terminate and advance if not at end of string */
-    if (c != '\0')
-	*ptr++ = '\0';
-   /* Skip trailing cr-nl pair */
-    if (c == '\n' || c == '\r') {
-	if ((*ptr == '\r' || *ptr == '\n') && *ptr != c)
-	    ptr++;
-    }
-   /* Save current location */
-
-    *str = ptr;
-    return arg;
-}
-
-/*
- * Scan command line arguments and collect options.
- */
-static int
-parseCmd(char *cmdstr)
-{
-    char               *arg;
-    char                msgbuff[MAX_PATH];
-
-   /* Skip command name */
-    getarg(&cmdstr);
-
-   /* Get rest of arguments */
-    while ((arg = getarg(&cmdstr)) != NULL) {
-	if (*arg == '-') {
-	    while (*++arg != '\0') {
-		switch (*arg) {
-		case 'i':	/* Image file name */
-		case 'I':
-		    if (imagename != NULL)
-			goto error;
-		    imagename = getarg(&cmdstr);
-		    break;
-		case 'o':	/* Object table size */
-		case 'O':
-		    if (!getnum(getarg(&cmdstr), &defotsize))
-			goto error;
-		    break;
-		case 'g':	/* Grow size */
-		case 'G':
-		    if (!getnum(getarg(&cmdstr), &growsize))
-			goto error;
-		    break;
-		default:
-		    goto error;
-		}
-	    }
-	} else {
-	    if (loadfile != NULL)
-	        goto error;
-	    loadfile = arg;
-	}
-    }
-
-    return 1;
-
-  error:
-
-    LoadString(GetModuleHandle(NULL), IDS_INVARG, msgbuff, sizeof(msgbuff));
-    strcat(msgbuff, arg);
-    MessageBox(NULL, msgbuff, szAppName, MB_OK | MB_ICONERROR);
-    return 0;
-
-}
-
-#else
 
 int
 main(int argc, char *argv[])
@@ -292,6 +46,7 @@ main(int argc, char *argv[])
     char               *str;
 
    /* Process arguments */
+    progname = *argv;
     while ((str = *++argv) != NULL) {
 	if (*str == '-') {
 	    while (*++str != '\0') {
@@ -320,6 +75,57 @@ main(int argc, char *argv[])
 	}
     }
 
+   /* Do OS specific initialization */
+    if (!initSystem()) {
+	errorStr("Unable to initialize system", NULL);
+	return -1;
+    }
+
+   /* Load default files */
+    if (imagename == NULL && loadfile == NULL) {
+	char		*temp;
+	char		*ptr;
+	char		*last;
+	int		id;
+
+	/* Allocate some work space */
+	if ((temp = (char *)malloc(strlen(progname) + 5)) == NULL) {
+	   errorStr("Out of memory", NULL);
+	   return -1;
+	}
+	strcpy(temp, progname);
+	ptr = &temp[strlen(temp)];
+	last = ptr;
+
+	/* Find last chunk of filename */
+	while(ptr != temp) {
+	    if (*ptr == '.') {
+		*ptr = '\0';
+	    }
+	    if (*ptr == '\\' || *ptr == '/') {
+		last = ptr;
+		/* advance to end of string */
+		while (*ptr != '\0')
+		    ptr++;
+		break;
+	    }
+	    ptr--;
+	}
+	strcpy(ptr, ".sti");
+	if ((id = file_open(temp, "r", &id)) >= 0) {
+		imagename = temp;
+	} else {
+		strcpy(last, "boot.st");
+		if ((id = file_open(temp, "r", &id)) >= 0) {
+			loadfile = temp;
+		} else {
+	    		errorStr("No image found", NULL);
+	    		return -1;
+		}
+	}
+	file_close(id);
+    }
+
    /* If there is a image file, load it */
     if (imagename != NULL) {
 	load_file(imagename);
@@ -329,15 +135,18 @@ main(int argc, char *argv[])
        /* Compile into system */
 	if (loadfile != NULL)
 	    parsefile(loadfile);
+	else {
+	    errorStr("No image found", NULL);
+	    return -1;
+	}
     }
     close_files();
     return 0;
 
   error:
-    fprintf(stderr, "Invalid argument: %s", str);
-    return 0;
+    errorStr("Invalid argument: %s", str);
+    return -1;
 }
-#endif
 
 static int
 getnum(char *str, int *value)
