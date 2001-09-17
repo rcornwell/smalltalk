c92b2f6baa4a96b5f4455e5025453ee8d4f0d92c
Richard Cornwell <rcornwell@github.com>
2001-09-17 20:19:55 +0000

    Got display and cursor support working.

--- win32.c
+++ win32.c
@@ -1,12 +1,15 @@
 /*
  * Smalltalk interpreter: Windows 32 interface.
  *
- * $Log: $
+ * $Log: win32.c,v $
+ * Revision 1.1  2001/08/18 16:22:56  rich
+ * Initial revision
+ *
  *
  */
 
 #ifndef lint
-static char        *rcsid = "$Id: $";
+static char        *rcsid = "$Id: win32.c,v 1.1 2001/08/18 16:22:56 rich Exp rich $";
 #endif
 
 #ifdef WIN32
@@ -28,10 +31,34 @@ static void	    fill_buffer(char c);
 static char        *szAppName = "SmallTalk";
 static HWND         hwnd = NULL;
 static HBITMAP      dispmap = NULL;
+static HCURSOR	    cursor = NULL;
 static int	    bmsize;
 static int	    foundevents;
 unsigned long      *display_bits = NULL;
+static int	    display_rows;
+static int	    display_cols;
+static unsigned long *display_image = NULL;
 static int	    needconsole = 1;
+
+static unsigned char reverse_map[256] = {
+/* 0,  1,   2,    3,   4,   5,   6,   7,   8,   9,   A,   B,  C,  D,  E,  F */
+0xFF,0x7F,0xBF,0x3F,0xDF,0x5F,0x9F,0x1F,0xEF,0x6F,0xAF,0x2F,0xCF,0x4F,0x8F,0x0F,
+0xF7,0x77,0xB7,0x37,0xD7,0x57,0x97,0x17,0xE7,0x67,0xA7,0x27,0xC7,0x47,0x87,0x07,
+0xFB,0x7B,0xBB,0x3B,0xDB,0x5B,0x9B,0x1B,0xEB,0x6B,0xAB,0x2B,0xCB,0x4B,0x8B,0x0B,
+0xF3,0x73,0xB3,0x33,0xD3,0x53,0x93,0x13,0xE3,0x63,0xA3,0x23,0xC3,0x43,0x83,0x03,
+0xFD,0x7D,0xBD,0x3D,0xDD,0x5D,0x9D,0x1D,0xED,0x6D,0xAD,0x2D,0xCD,0x4D,0x8D,0x0D,
+0xF5,0x75,0xB5,0x35,0xD5,0x55,0x95,0x15,0xE5,0x65,0xA5,0x25,0xC5,0x45,0x85,0x05,
+0xF9,0x79,0xB9,0x39,0xD9,0x59,0x99,0x19,0xE9,0x69,0xA9,0x29,0xC9,0x49,0x89,0x09,
+0xF1,0x71,0xB1,0x31,0xD1,0x51,0x91,0x11,0xE1,0x61,0xA1,0x21,0xC1,0x41,0x81,0x01,
+0xFE,0x7E,0xBE,0x3E,0xDE,0x5E,0x9E,0x1E,0xEE,0x6E,0xAE,0x2E,0xCE,0x4E,0x8E,0x0E,
+0xF6,0x76,0xB6,0x36,0xD6,0x56,0x96,0x16,0xE6,0x66,0xA6,0x26,0xC6,0x46,0x86,0x06,
+0xFA,0x7A,0xBA,0x3A,0xDA,0x5A,0x9A,0x1A,0xEA,0x6A,0xAA,0x2A,0xCA,0x4A,0x8A,0x0A,
+0xF2,0x72,0xB2,0x32,0xD2,0x52,0x92,0x12,0xE2,0x62,0xA2,0x22,0xC2,0x42,0x82,0x02,
+0xFC,0x7C,0xBC,0x3C,0xDC,0x5C,0x9C,0x1C,0xEC,0x6C,0xAC,0x2C,0xCC,0x4C,0x8C,0x0C,
+0xF4,0x74,0xB4,0x34,0xD4,0x54,0x94,0x14,0xE4,0x64,0xA4,0x24,0xC4,0x44,0x84,0x04,
+0xF8,0x78,0xB8,0x38,0xD8,0x58,0x98,0x18,0xE8,0x68,0xA8,0x28,0xC8,0x48,0x88,0x08,
+0xF0,0x70,0xB0,0x30,0xD0,0x50,0x90,0x10,0xE0,0x60,0xA0,0x20,0xC0,0x40,0x80,0x00,
+};
 
 #define ID_TICKTIMER	1
 
@@ -39,15 +66,121 @@ static int	    needconsole = 1;
 int
 initSystem()
 {
+    /* Initialize the event queues. */
+    init_event(&input_queue);
+    init_event(&asyncsigs);
+
     return 1;
 }
+
+/* Shutdown the system */
+void
+endSystem()
+{
+    if (cursor != NULL)
+	DestroyCursor(cursor);
+}
+
 
 /* Become the cursor */
 Objptr
 BeCursor(Objptr op)
 {
+    int                 height, width;
+    int			cheight, cwidth;
+    int                 rwidth;
+    int                 i, j;
+    Objptr              offset;
+    int                 hot_x, hot_y;
+    unsigned char       *main_bits, *mask_bits;
+    unsigned char       *src_ptr, *dst_ptr, *msk_ptr;
+    unsigned char       *bits, *mask;
+    HINSTANCE           hInst;
+
+    /* Remove old cursor if there is one */
+    if (cursor != NULL) {
+	DestroyCursor(cursor);
+	cursor = NULL;
+    }
+
+    /* Set new cursor */
     cursor_object = op;
     rootObjects[CURSOBJ] = op;
+
+    /* If cursor is NilPtr, just return it */
+    if (op == NilPtr) 
+	return op;
+
+    /* Get max size */
+    cwidth = GetSystemMetrics(SM_CXCURSOR);
+    cheight = GetSystemMetrics(SM_CYCURSOR);
+
+    /* Extract cursor information */
+    height = get_integer(op, FORM_HEIGHT);
+    width = get_integer(op, FORM_WIDTH);
+    rwidth = ((width & 0x1f) == 0) ? width : ((width | 0x1f) + 1);
+    offset = get_pointer(op, FORM_OFFSET);
+    hot_x = get_integer(offset, XINDEX);
+    hot_y = get_integer(offset, YINDEX);
+    offset = get_pointer(op, FORM_BITMAP);
+    main_bits = (unsigned char *) (get_object_base(offset) +
+				   (fixed_size(offset) * sizeof(Objptr)));
+    offset = get_pointer(get_pointer(op, FORM_MASK), FORM_BITMAP);
+    mask_bits = (unsigned char *) (get_object_base(offset) +
+				   (fixed_size(offset) * sizeof(Objptr)));
+
+
+    /* Create local space for cursor */
+    i = (cwidth / 8) * cheight;
+    if ((bits = (unsigned char *) malloc(i)) == NULL)
+	return cursor_object;
+    if ((mask = (unsigned char *) malloc(i)) == NULL) {
+	free(bits);
+	return op;
+    }
+
+    src_ptr = main_bits;
+    msk_ptr = mask_bits;
+    dst_ptr = bits;
+    for(i = 0; i < cheight; i++ ) {
+	if (i < height) {
+	   for(j = 0; j < rwidth; j+= 8 ) {
+	      if (j <= cwidth) 
+		   *dst_ptr++ = reverse_map[*src_ptr]^(~reverse_map[*msk_ptr]);
+	      src_ptr++;
+	      msk_ptr++;
+	   }
+	} else 
+	   j = 0;
+
+	for(; j < cwidth; j += 8)
+	   *dst_ptr++ = 0xff;
+    }
+
+    /* now build mask */
+    src_ptr = mask_bits;
+    dst_ptr = mask;
+    for(i = 0; i < cheight; i++ ) {
+	if (i < height) {
+	   for(j = 0; j < rwidth; j+= 8 ) {
+	      if (j <= cwidth) 
+		   *dst_ptr++ = ~reverse_map[*src_ptr];
+	      src_ptr++;
+	   }
+	} else 
+	   j = 0;
+
+	for(; j < cwidth; j += 8)
+	   *dst_ptr++ = 0x00;
+    }
+
+    /* Create a new cursor */
+    hInst = GetModuleHandle(NULL);
+    cursor = CreateCursor(hInst, hot_x, hot_y, cwidth, cheight, bits, mask);
+
+    /* Free temp stuff */
+    free(bits);
+    free(mask);
     return op;
 }
 
@@ -86,7 +219,7 @@ BeDisplay(Objptr op)
 	RegisterClassEx(&wndclass);
 
 	hwnd = CreateWindow(szAppName, "Smalltalk", WS_OVERLAPPEDWINDOW,
-			    CW_USEDEFAULT, CW_USEDEFAULT, 500, 400,
+			    CW_USEDEFAULT, CW_USEDEFAULT, 512, 512,
 			    NULL, NULL, hInst, (PSTR) NULL);
 
 	/* Initialize rest */
@@ -96,20 +229,140 @@ BeDisplay(Objptr op)
     return op;
 }
 
+/* Copy the display image from display_bits to dislay_image */
+static void
+copyimage(HDC hdc, int x, int y, int w, int h)
+{
+    HDC                 hdcMem;
+    int                 nBytes;
+    int			nRows;
+    unsigned char      *src_ptr, *dest_ptr;
+    int                 row, index, delta;
+
+    /* Clip dimensions to within screen. */
+    if (x < 0)
+        x = 0;
+    if (y < 0)
+        y = 0;
+    if (x > display_cols)
+	x = display_cols;
+    if (y > display_rows)
+	y = display_rows;
+    if ((x + w - 1) > display_cols)
+	w = display_cols - x - 1;
+    if ((y + h - 1) > display_rows)
+	h = display_rows - y - 1;
+
+    /* Merge masks if needed */
+    nBytes = 1 + ((w - 1 + x) / 32) - (x / 32);
+
+    /* Calculate starting offsets and increments */
+    row = (display_cols + 30 /* - 1 + 31 */ ) / 32;
+    index = (y * row) + (x / 32);
+    dest_ptr = (unsigned char *)&display_image[index];
+    src_ptr = (unsigned char *)&display_bits[index];
+    delta = row - nBytes;
+
+    /* Convert delta and nBytes to bytecounts */
+    nBytes *= 4;
+    delta *= 4;
+
+    /* Interloop */
+    for (nRows = h; nRows > 0; nRows--) {	/* Vertical loop */
+	int		word;
+
+	word = nBytes;
+        while(word > 0) {	/* Horizontal loop */
+	    switch(word & 0x1F) {
+	    case 0:
+		   word--;
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 31:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 30:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 29:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 28:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 27:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 26:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 25:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 24:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 23:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 22:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 21:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 20:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 19:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 18:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 17:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 16:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 15:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 14:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 13:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 12:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 11:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 10:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 9:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 8:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 7:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 6:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 5:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 4:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 3:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 2:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    case 1:
+		   *dest_ptr++ = reverse_map[*src_ptr++];
+	    }
+	    word -= word & 0x1f;
+	}
+	dest_ptr += delta;
+	src_ptr += delta;
+    }
+
+    /* Update the image */
+    SetMapMode(hdc, MM_TEXT);
+    hdcMem = CreateCompatibleDC(hdc);
+    SelectObject(hdcMem, dispmap);
+    SetBitmapBits(dispmap, bmsize, display_image);
+
+    BitBlt(hdc, x, y, w, h, hdcMem, x, y, SRCCOPY);
+    DeleteDC(hdcMem);
+}
+
+
 void
 UpdateDisplay(Copy_bits blit_data)
 {
-    HDC                 hdcMem;
     HDC                 hdc = GetDC(hwnd);
 
-    SetBitmapBits(dispmap, bmsize, display_bits);
-    hdcMem = CreateCompatibleDC(hdc);
-    SelectObject(hdcMem, dispmap);
-
-    BitBlt(hdc, blit_data->dx, blit_data->dy,
-	   blit_data->h, blit_data->w,
-	   hdcMem, blit_data->dx, blit_data->dy, SRCINVERT);
-    DeleteDC(hdcMem);
+    copyimage(hdc, blit_data->dx, blit_data->dy, blit_data->w, blit_data->h);
     ReleaseDC(hwnd, hdc);
 }
 
@@ -138,10 +391,6 @@ WaitEvent(int suspend)
 		    if (inrec.EventType == KEY_EVENT &&
 			inrec.Event.KeyEvent.bKeyDown &&
 			inrec.Event.KeyEvent.uChar.AsciiChar != 0) {
-			char	buffer[1000];
-			wsprintf(buffer, "Read '%c'", 
-		        	inrec.Event.KeyEvent.uChar.AsciiChar);
-			dump_string(buffer);
 		        fill_buffer(inrec.Event.KeyEvent.uChar.AsciiChar);
 		        foundevents = 1;
 		    }
@@ -182,7 +431,6 @@ WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
     static int          buttons = 0;
     PAINTSTRUCT         ps;
     int                 row, col;
-    static BITMAP       bm;
 
     switch (iMsg) {
     case WM_CREATE:
@@ -199,53 +447,48 @@ WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	    int                 crow, ccol;
 	    int                 roundcol;
 	    unsigned long	*temp;
-	    unsigned long	*temp2;
+	    char		buffer[1024];
 
 	    crow = get_integer(display_object, FORM_HEIGHT);
 	    ccol = get_integer(display_object, FORM_WIDTH);
-	    if (crow == row && ccol == col)
+	    if (crow == row && ccol == col && dispmap != NULL)
 		return 0;
-	    if ((col & 0x1f) != 0)
-		roundcol = 1 + (col | 0x1f);
-	    else
-		roundcol = col;
+
+	wsprintf(buffer, "Size %d, %d\n", col, row);
+	dump_string(buffer);
+
+	    roundcol = (col + 31) & ~0x1f;
 	    if (dispmap != NULL)
 		DeleteObject(dispmap);
 	    bmsize = (roundcol / 8) * row;
 	    dispmap = CreateBitmap(roundcol, row, 1, 1, NULL);
 	    if ((temp = (unsigned long *)malloc(bmsize)) == NULL)
 		break;
-	    temp2 = display_bits;
-#if 0
-	    GetObject(dispmap, sizeof(BITMAP), (LPVOID) & bm);
-	    display_bits = (unsigned long *) bm.bmBits;
-#endif
+	    if (display_bits != NULL)
+	        free(display_bits);
+	    display_bits = temp;
+
+	    if ((temp = (unsigned long *)malloc(bmsize)) == NULL)
+		break;
+	    if (display_image != NULL)
+	        free(display_image);
+	    display_image = temp;
+
+	    display_rows = row;
+	    display_cols = col;
 	    Set_integer(display_object, FORM_HEIGHT, row);
 	    Set_integer(display_object, FORM_WIDTH, col);
-	    display_bits = temp;
 	    PostEvent(EVENT_RESIZE, 0);
 	    foundevents = 1;
-	    if (temp2 != NULL)
-		free(temp2);
 	}
 	break;
 
     case WM_PAINT:
-
 	BeginPaint(hwnd, &ps);
 	if (dispmap != NULL) {
-	    HDC                 hdcMem;
-
-	    SetBitmapBits(dispmap, bmsize, display_bits);
-
-	    hdcMem = CreateCompatibleDC(ps.hdc);
-	    SelectObject(hdcMem, dispmap);
-
-	    BitBlt(ps.hdc, ps.rcPaint.top, ps.rcPaint.left,
-		   ps.rcPaint.bottom - ps.rcPaint.top,
+    	    copyimage(ps.hdc, ps.rcPaint.left, ps.rcPaint.top,
 		   ps.rcPaint.right - ps.rcPaint.left,
-		   hdcMem, ps.rcPaint.top, ps.rcPaint.left, SRCINVERT);
-	    DeleteDC(hdcMem);
+		   ps.rcPaint.bottom - ps.rcPaint.top);
 	}
 	EndPaint(hwnd, &ps);
 	return 0;
@@ -278,12 +521,19 @@ WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	buttons &= ~4;
 
       postmouse:
-	PostEvent(EVENT_MOUSEBUT, buttons);
-
-    case WM_MOUSEMOVE:
 	col = LOWORD(lParam);
 	row = HIWORD(lParam);
-	PostEvent(EVENT_MOUSEPOS, ((col & 0xfff) << 12) + (row & 0xfff));
+	PostEvent(EVENT_MOUSEPOS, ((row & 0xfff) << 12) + (col & 0xfff));
+	PostEvent(EVENT_MOUSEBUT, buttons);
+	foundevents = 1;
+	return 0;
+
+    case WM_MOUSEMOVE:
+	if (cursor != NULL)
+	    SetCursor(cursor);
+	col = LOWORD(lParam);
+	row = HIWORD(lParam);
+	PostEvent(EVENT_MOUSEPOS, ((row & 0xfff) << 12) + (col & 0xfff));
 	foundevents = 1;
 	return 0;
 
@@ -581,13 +831,20 @@ static void
 fill_buffer(char c)
 {
     DWORD	did;
+    HANDLE	out;
 
     out = GetStdHandle(STD_OUTPUT_HANDLE);
     WriteFile(out, &c, 1, &did, 0);
-    if (c == '\r') 
+    if (c == '\r') {
         c = '\n';
+        WriteFile(out, &c, 1, &did, 0);
+    }
+ 
     if (console.file_buffer != NULL) {
-	if (console.file_len < BUFSIZE)
+	if (c == '\b' && console.file_len > 0) {
+	    console.file_len--;
+	    WriteFile(out, " \b", 2, &did, 0);
+	} else if (console.file_len < BUFSIZE)
 	    console.file_buffer[console.file_len++] = c;
 	if (console.file_len == BUFSIZE || c == '\n' ||
 	    console.file_flags & FILE_CHAR)
